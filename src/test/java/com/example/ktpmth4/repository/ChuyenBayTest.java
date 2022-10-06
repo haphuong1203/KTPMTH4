cd28488123bf026f94995e8769360a25663c9087
haphuong1203 <haphuongminyoongi@gmail.com>
2022-10-06 11:50:29 +0700

    cau6

--- src/test/java/com/example/ktpmth4/repository/ChuyenBayTest.java
+++ src/test/java/com/example/ktpmth4/repository/ChuyenBayTest.java
@@ -23,5 +23,10 @@ public class ChuyenBayTest {
         System.out.println(chuyenBayRepository.findChuyenBayByGaDiVaGaDen());
     }
 
-   
+    @Test
+    void cau6(){
+        System.out.println("Có " + chuyenBayRepository.findChuyenBayByGaDi() + " chuyến bay xuất phát từ Sài Gòn");
+    }
+
+
 }
